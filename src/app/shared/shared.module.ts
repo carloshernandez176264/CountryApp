d53bc0aa719390b5018143d20ebcac2dcebd4227
carloshernandez176264 <carloshernandez176264@hotmail.com>
2023-05-08 11:27:34 -0500

    first commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { HomePageComponent } from './pages/homePage/home-page/home-page.component';
+import { AboutPageComponent } from './pages/aboutPage/about-page/about-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+
+
+
+
+
+@NgModule({
+  declarations: [
+    HomePageComponent,
+    AboutPageComponent,
+    SidebarComponent,
+    ContactPageComponent,
+    SearchBoxComponent,
+
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+  ],
+  exports: [
+    HomePageComponent,
+    AboutPageComponent,
+    SidebarComponent,
+    ContactPageComponent,
+    SearchBoxComponent,
+
+  ]
+})
+export class SharedModule { }
